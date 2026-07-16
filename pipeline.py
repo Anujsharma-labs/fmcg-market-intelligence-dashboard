437a38004a517e4d764d6ed77a091911784219cf
Anujsharma-labs <as8895411@gmail.com>
2026-07-17 05:26:29 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -2,8 +2,8 @@ def run_pipeline():
 
     import requests
     import pandas as pd
-    import streamlit as st
 
+    import streamlit as st
     API_KEY = st.secrets["API_KEY"]
 
     from llm_filter import is_relevant
@@ -38,19 +38,13 @@ def run_pipeline():
 
     print("Status Code:", response.status_code)
 
+    # JSON -> Python Dictionary
     data = response.json()
-
-    if response.status_code != 200:
-        raise Exception(f"HTTP Error: {response.status_code} - {data}")
-
-    if "articles" not in data:
-        raise Exception(f"NewsAPI Response: {data}")
 
     # Create DataFrame
     news = []
 
     for article in data["articles"]:
-
         news.append({
             "source": article["source"]["name"],
             "title": article["title"],
@@ -60,13 +54,9 @@ def run_pipeline():
         })
 
     df = pd.DataFrame(news)
-
     df.to_csv("data/news.csv", index=False)
 
-    # -----------------------------
     # Remove Duplicates
-    # -----------------------------
-
     print("Original Articles:", len(df))
 
     clean_df = remove_exact_duplicates(df)
@@ -77,40 +67,30 @@ def run_pipeline():
 
     clean_df.to_csv("data/clean_news.csv", index=False)
 
-    # -----------------------------
-    # Keyword Filtering
-    # -----------------------------
-
+    # LLM Filtering
     relevant_news = []
 
     for _, row in clean_df.iterrows():
 
-        title = row["title"] if pd.notna(row["title"]) else ""
-        description = row["description"] if pd.notna(row["description"]) else ""
-
-        if is_relevant(title, description):
+        if is_relevant(
+            row["title"],
+            row["description"]
+        ):
             relevant_news.append(row)
 
     relevant_df = pd.DataFrame(relevant_news)
 
     print("Relevant Articles:", len(relevant_df))
 
-    # If nothing relevant found
-    if relevant_df.empty:
-        return "# No relevant FMCG news found today."
-
-    # -----------------------------
     # Credibility Score
-    # -----------------------------
+    # relevant_df = add_credibility_score(relevant_df)
 
-    relevant_df = add_credibility_score(relevant_df)
-
-    # -----------------------------
     # Generate Newsletter
-    # -----------------------------
-
     newsletter = generate_newsletter(relevant_df)
 
+    print(newsletter)
+
+    # Save Newsletter
     with open("newsletter.md", "w", encoding="utf-8") as f:
         f.write(newsletter)
 
